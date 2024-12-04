1b89804750da37c06ddc38f47b9d58e44dcc2299
Gebruiker <Gebruiker@GEBRUIK-VQGG0SI>
2024-12-04 15:22:26 +0100

    Ajoutez des fichiers projet.

---
+++ TextEditor/Models/Doc.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TextEditor.Models
+{
+    public class Doc
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public string? UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public IdentityUser? User { get; set; }
+    }
+}

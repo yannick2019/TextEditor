4b3e4abf3559f9c4f5acbbc148b6cb6186d3fc2b
Yannick <cleps2012@gmail.com>
2024-12-06 16:32:33 +0100

    change primary color to dark

--- TextEditor/Controllers/DocsController.cs
+++ TextEditor/Controllers/DocsController.cs
@@ -40,8 +40,6 @@ namespace TextEditor.Controllers
         }
 
         // POST: Docs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Content,UserId")] Doc doc)
@@ -83,8 +81,6 @@ namespace TextEditor.Controllers
         }
 
         // POST: Docs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,UserId")] Doc doc)
